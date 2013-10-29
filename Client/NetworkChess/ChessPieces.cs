1640f9118f70f628a794556398e8205968645538
crodeheaver <crodeheaver@5fca7196-5785-4d20-a8a3-7342520914f7>
2013-10-29 17:03:57 +0000

    I added the beginnings of our client-sever protocol (Checkmate!)

---
+++ Client/NetworkChess/ChessPieces.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Board_piece
+{
+    class ChessPieces
+    {
+        // Animation that respresents the piece
+        public Texture2D piecetexture;
+        //Position of the chess piece relative to the upper left side of the screen
+        public Vector2 Position;
+        //State of the piece
+        public bool Active;
+        public int Width { get { return piecetexture.Width; } }
+        public int Height { get { return piecetexture.Height; } }
+        public void Initialize(Texture2D texture, Vector2 position)
+        {
+            piecetexture = texture;
+            //Starting position of the piece (around the middle of the screen and to the back)
+            Position = position;
+            //Chess piece is visible
+            Active = true;
+        }
+        public void Update()
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(piecetexture, Position, null, Color.White, 0f, Vector2.Zero, 1f,
+                    SpriteEffects.None, 0f);
+        }
+    }
+}
